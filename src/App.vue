44a72576869334834785b5436f5fc1ce0980a60e
Chavez Harris <codedbychavez@gmail.com>
2024-05-14 18:10:11 -0400

    Complete core update

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,119 @@
 <template>
   <main id="main">
-    <button class="btn">
-      Calculate Age
-    </button>
+    <h1>Age Calculator</h1>
 
-    <FeatureWrapper>
+    <FeatureWrapper feature-key="YOUR-FEATURE-FLAG-KEY">
 
       <div>
-      <p class="text">Calculate your age below by providing your year of birth</p>
-      <input type='number' v-model='appData.birthYear' />
-      <button class='btn btn-calculate' @click='calculateUserAge()'>
-        Calculate
-      </button>
+        <p class="text">Calculate your age by providing your year of birth.</p>
+        <input type='number' v-model='appData.birthYear' />
+        <button class='button button-calculate' @click='calculateUserAge()' :disabled="!isYearValid">
+          Calculate
+        </button>
 
-      <p>You are {{ appData.age }} years old</p>
-    </div>
+        <p v-if="appData.age">You are {{ appData.age }} years old!</p>
+
+        <p v-else>Enter your year of birth above and press calculate!</p>
+      </div>
 
       <template #else>
-        Sorry, This feature has been disabled by the Admin
+        Sorry, This feature has been disabled by the Admin.
+      </template>
+
+      <template #loading>
+        <p>Loading...</p>
       </template>
 
     </FeatureWrapper>
-
-
 
   </main>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-
+import { computed, reactive } from 'vue';
+import { FeatureWrapper } from 'configcat-vue';
 
 const appData = reactive({
   userAgeFeature: null,
   error: null,
-  birthYear: 0,
-  age: 0,
+  birthYear: '',
+  age: '',
+  errorMessage: '',
 });
 
+const yearRegex = /^(?:19|20)\d{2}$/;
+
 const calculateUserAge = () => {
-  appData.age = new Date().getFullYear() - appData.birthYear;
-  appData.birthYear = 0;
+  appData.errorMessage = '';
+  const userAge = new Date().getFullYear() - Number(appData.birthYear);
+  appData.age = userAge.toString();
+  appData.birthYear = '';
 }
+
+const isYearValid = computed((): boolean => {
+  return yearRegex.test(appData.birthYear);
+})
 
 </script>
 
 <style scoped>
 #main {
-	text-align: center;
-	 background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+  text-align: center;
+  background-color: #21D4FD;
+  background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.btn {
-	color: rgb(16, 82, 54);
-	  background-color: royalblue;
-	border: none;
-	 padding: 8px 16px;
-	border-radius: 15px;
-	font-size: 1.2rem;
-	cursor: pointer;
-	font-weight: 600;
+h1 {
+  color: rgb(238 242 255);
 }
 
-.btn:hover {
-	background-color: rgb(16, 82, 54);
-	color: #FFF;
+.button {
+  color: black;
+  background-color: orange;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 15px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  font-weight: 600;
 }
 
-.btn:focus-visible,
+.button:hover {
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+}
+
+.button:focus-visible,
 input:focus-visible {
-	outline: none;
+  outline: none;
 }
 
-.btn-calculate {
-	padding: 10px 25px;
-	margin-left: 8px;
-	border-radius: 8px;
+.button:disabled {
+  cursor: not-allowed;
+  background-color: lightgray;
+  color: darkgray;
+}
+
+.button-calculate {
+  padding: 10px 25px;
+  margin-left: 8px;
+  border-radius: 8px;
 }
 
 input {
-	padding: 10px 25px;
-	font-size: 1.2rem;
-	border-radius: 8px;
-	border: none;
+  padding: 10px 25px;
+  font-size: 1.2rem;
+  border-radius: 8px;
+  border: none;
 }
 
 .text {
-	font-size: 1.5em;
-	color: #FFF;
+  font-size: 1.5em;
+  color: #FFF;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { ConfigCatPlugin, PollingMode } from 'configcat-vue'
+
+const app = createApp(App);
+
+app.use(ConfigCatPlugin, {
+  sdkKey: "YOUR-CONFIGCAT-SDK-KEY",
+  pollingMode: PollingMode.AutoPoll, // Optional. Default is AutoPoll
+  clientOptions: {
+    pollIntervalSeconds: 5 // Optional. Specify the polling interval in seconds. The default is 60 seconds.
+  }
+})
+
+app.mount('#app')
